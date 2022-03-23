dccbfd493a53feba71236f7a6a913a73ab7f06c7
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-03-24 00:46:42 +0100

    ts

---
+++ main.ts
@@ -0,0 +1,17 @@
+// const character = 'luigi';
+
+// console.log(character);
+
+// const inputs = document.querySelectorAll('input');
+
+// console.log(inputs);
+
+// inputs.forEach(input => {
+//     console.log(input)
+// })
+
+const circ = (diameter:number) => {
+    return diameter * Math.PI;
+}
+
+console.log(circ(7.5));
