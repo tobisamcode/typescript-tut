69bc181e114dcafe25f647c0e140c0b55e5d26ab
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-03-24 06:44:17 +0100

    ts

--- main.ts
+++ main.ts
@@ -15,3 +15,58 @@ const circ = (diameter:number) => {
 }
 
 console.log(circ(7.5));
+
+// explicit types
+let characters: string;
+
+let age: number;
+
+let isLoggedIn: boolean;
+
+// age = 'luigi';
+age = 25;
+
+// isLoggedIn = 25;
+isLoggedIn = true;
+
+
+
+// arrays
+
+let ninjas: string[];
+
+// ninjas = [10, 21];
+ninjas = ['Tobi', 'sam'];
+
+ninjas.push('smith')
+console.log(ninjas);
+
+
+// union
+
+let mixed: (string | number)[] = [];
+mixed.push('hello');
+mixed.push(25);
+// mixed.push(false);
+console.log(mixed);
+
+
+let uid: string | number;
+uid = 123;
+uid = "tboy";
+
+
+
+// Objects
+
+let ninjaOne: object;
+ninjaOne = {name: 'Yushi', age: 20};
+
+let ninjaTwo: {
+    name: string,
+    age: number,
+    beltColor: string
+}
+
+ninjaTwo = {name: 'mario', age: 30, beltColor: "pink"};
+
