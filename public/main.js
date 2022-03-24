cda0f5e14774138432daa32286d719510dc42be8
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-03-24 12:51:34 +0100

    ts

--- public/main.js
+++ public/main.js
@@ -64,7 +64,13 @@ let greet = () => {
 };
 greet();
 // greet = 'hello'
-const add = (a, b) => {
+const add = (a, b, c) => {
     console.log(a + b);
+    console.log(c);
 };
-add(5, 10);
+add(5, 10, '20');
+const minus = (a, b) => {
+    return a - b;
+};
+let result = minus(10, 7);
+// result = 'something'
