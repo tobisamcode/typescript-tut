c10b4e2c2767c81cb6bd980b0abf1e0fa1230f92
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-03-24 06:44:21 +0100

    ts

--- main.js
+++ main.js
@@ -9,3 +9,31 @@ var circ = function (diameter) {
     return diameter * Math.PI;
 };
 console.log(circ(7.5));
+// explicit types
+var characters;
+var age;
+var isLoggedIn;
+// age = 'luigi';
+age = 25;
+// isLoggedIn = 25;
+isLoggedIn = true;
+// arrays
+var ninjas;
+// ninjas = [10, 21];
+ninjas = ['Tobi', 'sam'];
+ninjas.push('smith');
+console.log(ninjas);
+// union
+var mixed = [];
+mixed.push('hello');
+mixed.push(25);
+// mixed.push(false);
+console.log(mixed);
+var uid;
+uid = 123;
+uid = "tboy";
+// Objects
+var ninjaOne;
+ninjaOne = { name: 'Yushi', age: 20 };
+var ninjaTwo;
+ninjaTwo = { name: 'mario', age: 30, beltColor: "pink" };
