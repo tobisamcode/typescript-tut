00f0857d3ec2ada0aa23e7f1824d5dfc35994c39
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-03-25 12:42:19 +0100

    ts

--- src/classes/Invoice.ts
+++ src/classes/Invoice.ts
@@ -1,9 +1,7 @@
-// classes
-export class Invoice {
-    // readonly client: string;
-    // private details: string;
-    // public amount: number;
+import { HasFormatter } from '../interfaces/HasFormatter.js'
 
+// classes
+export class Invoice implements HasFormatter {
     constructor(
         readonly client: string,
         private details: string,
