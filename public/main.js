7bc77868c2492b34e27ea422129a17a6954f56e3
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-03-26 00:46:19 +0100

    ts

--- public/main.js
+++ public/main.js
@@ -14,12 +14,15 @@ const ul = document.querySelector('ul');
 const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    // tuples
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
     let doc;
     if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     list.render(doc, type.value, 'end');
 });
