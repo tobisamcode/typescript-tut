cfddd6d6e7f5ba130f3e02043c9e4e9907863c93
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-03-25 22:02:59 +0100

    ts

--- public/main.js
+++ public/main.js
@@ -22,3 +22,11 @@ form.addEventListener('submit', (e) => {
     }
     list.render(doc, type.value, 'end');
 });
+// GENERICS
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: 'yoshi', age: 40 });
+let docTwo = addUID;
+console.log(docOne);
