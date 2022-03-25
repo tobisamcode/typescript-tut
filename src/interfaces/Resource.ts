f8c36d8f6d8ebe091c5efc8d17432228c6511d74
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-03-26 00:28:52 +0100

    ts

--- src/interfaces/Resource.ts
+++ src/interfaces/Resource.ts
@@ -1,17 +1,10 @@
-interface Resource <T> {
+// enums
+
+export enum ResourceApp { NAME, AGE, GENDER, PRODUCT, COLOUR }; 
+
+export interface Resource <T> {
     uid: number;
-    resourceName: string;
+    resourceName: ResourceApp;
     data: T;
 }
 
-const docThree: Resource <object> = {
-    uid: 1,
-    resourceName: 'person',
-    data: {name: 'shaun'}
-}
-
-const docFour: Resource <string[]> = {
-    uid: 2,
-    resourceName: 'soppingList',
-    data: ['bread', 'milk', 'toilet roll']
-}
