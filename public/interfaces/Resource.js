2ba6f81f706ffd96055275e59dcd1b642584cd4b
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-03-25 22:03:10 +0100

    ts

---
+++ public/interfaces/Resource.js
@@ -0,0 +1,11 @@
+"use strict";
+const docThree = {
+    uid: 1,
+    resourceName: 'person',
+    data: { name: 'shaun' }
+};
+const docFour = {
+    uid: 2,
+    resourceName: 'soppingList',
+    data: ['bread', 'milk', 'toilet roll']
+};
